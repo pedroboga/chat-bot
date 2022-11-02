f0795efc8554a8c50c2ff3dfc9e121fe00ad153f
Pedro Boga <45229010+pedroboga@users.noreply.github.com>
2022-11-01 21:54:40 -0300

    Created project

---
+++ ChatBot/ChatBot/ChatBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatBotApp.swift
+//  ChatBot
+//
+//  Created by Pedro Boga on 01/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ChatBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
