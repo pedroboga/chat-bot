fd1fe4fae3fff37a6657d9a0b0dfb662fba8b4e9
Pedro Boga <45229010+pedroboga@users.noreply.github.com>
2022-11-01 22:05:02 -0300

    Initial layout of the app

--- ChatBot/ChatBot/ContentView.swift
+++ ChatBot/ChatBot/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var messageText = ""
+    @State var messages: [String] = ["Welcome to ChatBot"]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+                Text("ChatBot")
+                    .font(.largeTitle)
+                    .bold()
+                Image(systemName: "bubble.left.fill")
+                    .font(.system(size: 26))
+                    .foregroundColor(Color.blue)
+            }
+            
+            ScrollView {
+                //Messages
+            }
+            
+            HStack {
+                TextField("Type something", text: $messageText)
+                    .padding()
+                    .background(Color.gray.opacity(0.1))
+                    .cornerRadius(10)
+                    .onSubmit {
+                        // send message
+                    }
+                
+                Button {
+                    // send message
+                } label: {
+                    Image(systemName: "paperplane.fill")
+                        
+                }
+                .font(.system(size: 26))
+                .padding(.horizontal, 10)
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
